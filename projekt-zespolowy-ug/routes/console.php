6e6290549e7849d50f15796a91acb6834673a878
Karol Falkowski <karolfalkowski00@gmail.com>
2025-01-04 14:52:42 +0100

    Add task scheduler

--- projekt-zespolowy-ug/routes/console.php
+++ projekt-zespolowy-ug/routes/console.php
@@ -1,18 +1,11 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 
-app()->singleton(Schedule::class, function () {
-    return new Schedule();
-});
+Schedule::command('reminders:check')->everySecond();
 
-$schedule = app(Schedule::class);
 
-$schedule->command('reminders:check')->everyMinute();
